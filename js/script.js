58913ed0bb1b8fb08d3adf2fe994899ca5c00658
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-09 14:05:06 +0200

    take all input value

--- js/script.js
+++ js/script.js
@@ -3,18 +3,42 @@ console.log('ok js');
 
 // recupero elementi dal DOM
 const topElement = document.getElementById('top-element');
+
 const responseElement = document.getElementById('response');
+
 const firstinputelement = document.getElementById('user-answer-1');
+
 const secondinputelement = document.getElementById('user-answer-2');
+
 const thirdinputelement = document.getElementById('user-answer-3');
+
 const fourthinputelement = document.getElementById('user-answer-4');
+
 const fifthinputelement = document.getElementById('user-answer-5');
+
 const buttonelement = document.getElementById('button');
+
 const middleElement = document.getElementById('middle-element');
+
 const countdownElement = document.getElementById('countdown');
+
 const bottomElement = document.getElementById('bottom-element');
+
 const firstNumber = document.getElementById('first-number');
+
 const secondNumber = document.getElementById('second-number');
+
 const thirdNumber = document.getElementById('third-number');
+
 const fourthNumber = document.getElementById('fourth-number');
+
 const fifthNumber = document.getElementById('fifth-number');
+
+
+// recupero i valori degli elementi
+
+const firstvalue = parseInt(firstinputelement.value);
+const secondvalue = parseInt(secondinputelement.value);
+const thirdvalue = parseInt(thirdinputelement.value);
+const fourthtvalue = parseInt(fourthinputelement.value);
+const fifthvalue = parseInt(fifthinputelement.value);
