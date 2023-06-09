92ea72f88231c3c3b758dbb4c93b38428db1ed90
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-09 13:58:57 +0200

    take all element from DOM

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 //check console log
 console.log('ok js');
+
+// recupero elementi dal DOM
+const topElement = document.getElementById('top-element');
+const responseElement = document.getElementById('response');
+const firstinputelement = document.getElementById('user-answer-1');
+const secondinputelement = document.getElementById('user-answer-2');
+const thirdinputelement = document.getElementById('user-answer-3');
+const fourthinputelement = document.getElementById('user-answer-4');
+const fifthinputelement = document.getElementById('user-answer-5');
+const buttonelement = document.getElementById('button');
+const middleElement = document.getElementById('middle-element');
+const countdownElement = document.getElementById('countdown');
+const bottomElement = document.getElementById('bottom-element');
+const firstNumber = document.getElementById('first-number');
+const secondNumber = document.getElementById('second-number');
+const thirdNumber = document.getElementById('third-number');
+const fourthNumber = document.getElementById('fourth-number');
+const fifthNumber = document.getElementById('fifth-number');
